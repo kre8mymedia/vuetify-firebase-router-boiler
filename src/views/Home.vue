86895bcbc09105a6d20ec12394b6d8b8b574746b
unknown <kre8mymedia@gmail.com>
2019-12-08 22:33:02 -0600

    added carousel to dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,72 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 xs6 class="text-xs-center text-sm-right pr-2">
+        <v-btn large><router-link to="/meetups">Explore Meetups</router-link></v-btn>
+      </v-flex>
+      <v-flex xs12 xs6 class="text-xs-center text-sm-left">
+        <v-btn large><router-link to="/meetup/new">Organize Meetups</router-link></v-btn>
+      </v-flex>
+    </v-layout>
+    <br>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel
+          v-model="model"
+          :show-arrows="showArrows"
+          :hide-delimiters="hideDelimiters"
+          :cycle="cycle"
+        >
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :key="meetup"
+            :src="meetup.imageUrl"
+          >
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://images.pexels.com/photos/1557547/pexels-photo-1557547.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', id: 'ads;dsajof23n3', title: 'Meetup in New York' },
+        { imageUrl: 'https://images.pexels.com/photos/89432/pexels-photo-89432.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', id: 'ads;dfadsac', title: 'Meetup in London' },
+        { imageUrl: 'https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', id: 'adddass;dsajof23n3', title: 'Meetup in Paris' },
+        { imageUrl: 'https://images.pexels.com/photos/2193300/pexels-photo-2193300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', id: 'ads;dsajdasdof23n3', title: 'Meetup in Sydney' },
+        { imageUrl: 'https://images.pexels.com/photos/532263/pexels-photo-532263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', id: 'ads;dsassdjof23n3', title: 'Meetup in Rome' }
+      ],
+      colors: [
+        'primary',
+        'secondary',
+        'yellow darken-2',
+        'red',
+        'orange',
+      ],
+      model: 0,
+      showArrows: true,
+      hideDelimiters: false,
+      cycle: true,
+    }
+  },
 }
 </script>
+
+<style scoped>
+  .title {
+    text-align: center;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+  }
+</style>
